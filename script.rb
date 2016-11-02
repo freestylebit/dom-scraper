dc0c17ebfabba6f9fcb707bde62de67ce1e547de
Peter Cho <pcho231@gmail.com>
2016-11-02 00:37:59 -0400

    Stylized output

--- script.rb
+++ script.rb
@@ -12,6 +12,25 @@ puts
 print '--> Password: '
 pass = STDIN.noecho(&:gets).strip
 puts
+puts '--> Give me one sec...'
+puts
+puts
 
 scraper = Scraper.new(user, pass);
-puts scraper.query
+results = scraper.query
+puts
+puts '--------------------------------------'
+puts '            Account Summary           '
+puts '--------------------------------------'
+print 'Bill Amount       | ' + results['bill']
+puts
+print 'Bill Due Date     | ' + results['due_date']
+puts
+puts
+print 'Usage (kWh)       | ' + results['usage']
+puts
+print 'Meter Start Date  | ' + results['service_start']
+puts
+print 'Meter End Date    | ' + results['service_end']
+puts
+puts
