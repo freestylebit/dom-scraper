609314604b9cbe6c2681d2362815aae799c483f6
Peter Cho <pcho231@gmail.com>
2016-11-04 00:50:58 -0400

    Set up nokogori test

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -2,12 +2,20 @@
 
 load "lib/helper.rb"
 
+# Generate code coverage report.
+require 'simplecov'
+SimpleCov.start
+
 describe "Helper" do
   before :each do
     # Mock some generic class to expose the module.
-    @faux = Class.new { include Helper }
+    @faux = Class.new.extend(Helper)
+
+    # Prepare some sample HTML page for nokogiri to process.
+    @html = Nokogiri::HTML('<html><body><div id="sample">Unicorn</div></body></html>')
   end
 
   it "parses nokogiri DOM elements correctly" do
+    expect(@faux.parse_text(@html.at('#sample'))).to eq('Unicorn')
   end
 end
